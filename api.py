9fd234a6ca855395d50024e489d76255ccf3f3ad
hannahmjelde <hamje7270@uib.no>
2024-05-08 11:41:27 +0200

    Settet opp pythonfil for api endpoint

---
+++ api.py
@@ -0,0 +1,10 @@
+import json
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/api/data', methods=['GET'])
+
+def data_til_highcharts():
+    data: []
+    
+def data_til_filter():
+app.run()
